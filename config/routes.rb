1e40abae4e654945262fafd5a46eb9819c43e94f
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-07-21 20:59:45 -0400

    Updated index.html.erb
    config/routes was sorted.

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@
 Rails.application.routes.draw do
 
   # admin
-  get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: :tasks
 
-  # edit / update
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  patch 'tasks/:id', to: 'tasks#update'
-
-  # new / create
-  get 'tasks/new', to: 'tasks#new'
+  # create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
 
-  # show
-  get  'tasks/:id', to: 'tasks#show', as: "task"
+  # view
+  get  'tasks/:id', to: 'tasks#show', as: :task
+
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 
   # delete
   delete 'tasks/:id', to: 'tasks#destroy'
